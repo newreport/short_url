484535042ed6acbc77a418cb471ce2b5dd7826ba
newreport <haruki959@qq.com>
2022-09-22 15:38:02 +0800

    .

--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -18,7 +18,7 @@ const count = ref(0)
   </div>
 
   <p>
-    Check out
+    Check outDev
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
