cf1515a1796bfb9a2dd367df3ab5006be4b04814
newreport <haruki959@qq.com>
2022-09-26 21:35:35 +0800

    .

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,20 +1,8 @@
 // main.ts
 import { createApp } from 'vue'
 import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
-<<<<<<< HEAD
 const app= createApp(App)
-app.mount('#app')
-
-
-=======
-const app = createApp(App)
-
-app.use(ElementPlus)
-
-
 
 app.mount('#app')
->>>>>>> main
+
