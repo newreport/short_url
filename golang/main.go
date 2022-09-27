3f74f78a72a25dce50bb4c83cf2c1837f73035a2
newreport <haruki959@qq.com>
2022-09-27 21:13:55 +0800

    add beeogo project

--- golang/main.go
+++ golang/main.go
@@ -1,18 +1,56 @@
-﻿package main
- 
+package main
+
 import (
-    "github.com/astaxie/beego"  //导入beego依赖
+	"database/sql"
+	"fmt"
+	_ "golang/routers"
+	"os"
+
+	beego "github.com/beego/beego/v2/server/web"
+	_ "github.com/mattn/go-sqlite3"
 )
- 
-type MainController struct {
-    beego.Controller    //匿名包含beego.Controller
-}
- 
-func (this *MainController) Get() {
-    this.Ctx.WriteString("hello world")  //实现get响应
-}
- //https://cloud.tencent.com/developer/article/1541826
+
 func main() {
-    beego.Router("/", &MainController{})  //设置路由
-    beego.Run()   //启动Beego       
+
+	str, _ := os.Getwd()
+	fmt.Println(str)
+	InitOpen()
+
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
+
+// https://www.jianshu.com/p/c2f72fc0bdb5
+func InitOpen() {
+	db, err := sql.Open("sqlite3", "./user.db")
+	checkErr(err)
+
+	fmt.Println("创建数据表")
+	sql_table := `
+	CREATE TABLE IF NOT EXISTS "student"(
+		"name" VARCHAR(64) NULL,
+		"age" VARCHAR(64) NULL,
+		"class" VARCHAR(64) NULL
+	)`
+	db.Exec(sql_table)
+
+	//插入数据
+	stmt, err := db.Prepare("INSERT INTO student(name, age, class) values(?,?,?)")
+	checkErr(err)
+
+	res, err := stmt.Exec("小明", "12", "六年级一班")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+	fmt.Println(id)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
